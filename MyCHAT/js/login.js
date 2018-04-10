f7f9b0f7620ae11cc746e499e38d78a7f0c0738d
lilvedaes <daniela.venturo@utec.edu.pe>
2018-04-10 18:55:56 -0500

    Add files via upload

---
+++ MyCHAT/js/login.js
@@ -0,0 +1,11 @@
+function logIn() {
+    var u = document.forms["formuser"]["username"].value;
+    var p = document.forms["formpass"]["password"].value;
+    if (u == "Admin" && p == "Admin1") {
+        window.open("/Users/dani/Desktop/EVERYTHING/UTEC/2018-1/PLATAFORMAS/Ejercicios/MyCHAT/chat.html ","_self");
+    }
+    else {
+        var popup = document.getElementById("myPopup");
+        popup.classList.toggle("show");
+    }
+}
