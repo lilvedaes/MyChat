e37df770b7139f750bb9a8e36ddda3f01273d17f
Reynaldo Oscar Rojas Zelaya <reynaldo.rz.26@gmail.com>
2018-04-22 21:15:10 -0500

    Login y logout

--- labs/students/reynaldo.rojas/UI/app.py
+++ labs/students/reynaldo.rojas/UI/app.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, flash, render_template, session, request, redirect, session, abort
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def loginF():
-    return render_template('login.html')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('chats.html')
 
-@app.route('/chats')
-def chatsF():
-    return render_template('chats.html')
+@app.route('/login', methods = ['POST'])
+def login():
+    if request.form["password"] == 'Rojas' and request.form["username"] == 'Reynaldo':
+        session['logged_in'] = True
+    else:
+        flash('Wrong password')
+    return home()
+    
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return home()
 
 if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
     app.run(debug=True)
